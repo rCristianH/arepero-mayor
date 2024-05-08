d15f301e979b5c8f4d720c3826b8e58d1b8e35a8
Cristian Ramirez <ccrhdbj@outlook.com>
2024-05-08 13:15:05 -0500

    service worker

--- src/main.js
+++ src/main.js
@@ -15,3 +15,19 @@ $root.appendChild(About)
 $root.appendChild(Footer)
 
 Listeners()
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("../sw.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
